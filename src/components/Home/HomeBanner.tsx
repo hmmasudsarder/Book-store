75a17cc0a22824265b39c85f9d43be023ae19bee
hmmasudsarder <hmmasudsardar@gmail.com>
2025-01-27 01:59:11 +0600

    added responsive navbar or staticly

--- src/components/Home/HomeBanner.tsx
+++ src/components/Home/HomeBanner.tsx
@@ -4,15 +4,15 @@ import bg1 from "../../assets/bg/boi-col.jpg";
 
 const HomeBanner = () => {
   return (
-    <div className="container mx-auto h-screen px-4 mt-28 lg:mt-40">
-      <div className="flex flex-col md:flex-row items-center gap-5">
+    <div className="container mx-auto px-4 mt-28 lg:mt-40">
+      <div className="flex flex-col lg:flex-row items-center gap-5">
         <Slider />
-        <div className="flex lg:flex-col gap-3">
+        <div className="flex lg:flex-col gap-3 md:gap-6 lg:gap-3">
           <div className="h-[170px]">
-            <img src={bg1} alt=""  className="w-full h-full object-cover rounded-xl transition-all duration-300 ease-in-out transform scale-100 hover:scale-105"/>
+            <img src={bg1} alt=""  className="w-full md:w-[360px] h-full object-cover rounded-xl transition-all duration-300 ease-in-out transform scale-100 hover:scale-105"/>
           </div>
           <div className="h-[170px]">
-            <img src={bg2} alt="" className="w-full h-full object-cover rounded-xl transition-all duration-300 ease-in-out transform scale-100 hover:scale-105"/>
+            <img src={bg2} alt="" className="w-full md:w-[400px] h-full object-cover rounded-xl transition-all duration-300 ease-in-out transform scale-100 hover:scale-105"/>
           </div>
         </div>
       </div>
