91dd7a5870ef23d6d49be4babd7996943278e7f0
hmmasudsarder <hmmasudsardar@gmail.com>
2025-01-28 02:04:55 +0600

    added responsive copy right footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Header/Navbar";
 import Footer from "./components/Footer/Footer";
+import CopyrightFooter from "./components/Footer/CopyrightFooter";
 // { children }: { children: React.ReactNode })
-function App( ){
+function App() {
   return (
     <>
       <Navbar />
       <Outlet />
-      <Footer/>
+      <Footer />
+      <CopyrightFooter />
     </>
   );
 }
---
+++ src/components/Footer/CopyrightFooter.tsx
@@ -0,0 +1,15 @@
+const CopyrightFooter = () => {
+    return (
+      <div className="bg-black text-center py-3">
+        <p className="text-gray-400 text-sm">
+          Copyright © 2025 Book Shop. All rights reserved. Developed By
+          <a href="https://www.linkedin.com/in/hmmasudsarder/" className="text-yellow-500 font-semibold ml-1">
+            Masud Sarder IT
+          </a>
+        </p>
+      </div>
+    );
+  };
+  
+  export default CopyrightFooter;
+  
