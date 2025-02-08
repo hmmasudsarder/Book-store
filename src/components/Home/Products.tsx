c6ab64aac0fe8265d93668c9d9901fd4d48a7530
hmmasudsarder <hmmasudsardar@gmail.com>
2025-02-09 04:16:35 +0600

    added live link

--- src/components/Home/Products.tsx
+++ src/components/Home/Products.tsx
@@ -72,7 +72,7 @@ const Products = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-        {products?.map((product) => (
+        {products?.map((product: any) => (
           <div
             key={product.id}
             className="border rounded-xl p-2 shadow-sm hover:shadow-lg transition duration-500"
---
+++ src/swipper.d.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'swiper';
+declare module 'swiper/css/navigation' {
+    const styles: any;
+    export default styles;
+}
+declare module 'swiper/css/pagination' {
+    const styles: any;
+    export default styles;
+}
+declare module 'swiper/css/autoplay' {
+    const styles: any;
+    export default styles;
+}
+declare module 'swiper/css' {
+    const styles: any;
+    export default styles;
+}
