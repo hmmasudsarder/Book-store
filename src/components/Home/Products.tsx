7f84681e0159921bfe8e7dc773ea7b3a5ad48cd3
hmmasudsarder <hmmasudsardar@gmail.com>
2025-07-08 18:46:02 +0600

    setup full project using react js or tailwindcss

--- src/components/Home/Products.tsx
+++ src/components/Home/Products.tsx
@@ -46,14 +46,10 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 //   },
 // ];
 
-
 const Products = () => {
   const { data, isFetching } = useGetAllProductsQuery(undefined);
 
-  console.log(data)
-
   const products = data?.data || [];
-  console.log(products);
 
   if (isFetching)
     return (
@@ -79,48 +75,54 @@ const Products = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {products?.map((product: any) => (
-          <div
-            key={product.id}
-            className="border rounded-xl p-2 shadow-sm hover:shadow-lg transition duration-500"
-          >
-            <div className="relative group overflow-hidden">
-              <span className="absolute top-0 z-10 right-0 bg-blue-500 text-white text-xs px-2 py-[6px] rounded-xl">
-                {/* {product.discount} */}
-                Discount
-              </span>
-              <img
-                src={product?.productImg}
-                alt={product.name}
-                className="w-auto mx-auto h-48 object-contain group-hover:scale-105 transition-transform duration-500 "
-              />
-            </div>
-            <div className="flex items-center justify-between">
-              <h3 className="text-center text-lg font-medium mt-2">
-                {truncateName(product.name, 15)}
-              </h3>
-              <button className="text-black p-4 font-semibold">
+          <Link to={`/product/${product._id}`}>
+            <div
+              key={product.id}
+              className="border rounded-xl p-2 shadow-sm hover:shadow-lg transition duration-500"
+            >
+              <div className="relative group overflow-hidden">
+                <span className="absolute top-0 z-10 right-0 bg-blue-500 text-white text-xs px-2 py-[6px] rounded-xl">
+                  {/* {product.discount} */}
+                  Discount
+                </span>
+                <img
+                  src={product?.productImg}
+                  alt={product.name}
+                  className="w-auto mx-auto h-48 object-contain group-hover:scale-105 transition-transform duration-500 "
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <h3 className="text-center text-lg font-medium mt-2">
+                  {truncateName(product.name, 15)}
+                </h3>
+                {/* <button className="text-black p-4 font-semibold">
                 ⭐ 4.5
-              </button>
+              </button> */}
+                <div className="text-center text-gray-500">
+                  <span className="line-through text-sm">
+                    $ {product?.price - 30}
+                  </span>{" "}
+                  <span className="text-blue-600 font-semibold">
+                    $ {product.price}
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <Link
+                  to={`/product/${product._id}`}
+                  className="flex items-center justify-between gap-4"
+                >
+                  <button className=" flex-grow bg-pink-500 text-white py-[6px] mt-3 mb-2 rounded-xl hover:bg-gradient-to-br from-blue-700 to-blue-400 transition duration-500 transform">
+                    View Details
+                  </button>
+                  <button className="font-semibold text-lg border-2 border-gray-500 rounded-full p-2">
+                    <MdOutlineShoppingCart className="text-lg" />
+                  </button>
+                </Link>
+              </div>
             </div>
-            <div className="text-center text-gray-500">
-              <span className="line-through text-sm">
-                ৳ {product?.price - 30}
-              </span>{" "}
-              <span className="text-blue-600 font-semibold">
-                ৳ {product.price}
-              </span>
-            </div>
-            <div >
-              <Link to={`/product/${product._id}`} className="flex items-center justify-between gap-4">
-                <button className=" flex-grow bg-pink-500 text-white py-[6px] mt-3 mb-2 rounded-xl hover:bg-gradient-to-br from-blue-700 to-blue-400 transition duration-500 transform">
-                  View Details
-                </button>
-                <button className="font-semibold text-lg border-2 border-gray-500 rounded-full p-2">
-                  <MdOutlineShoppingCart className="text-lg" />
-                </button>
-              </Link>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
